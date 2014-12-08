eba6740c4d896e93c623a86623b28936295d453f
kvnh <kevinhackett72@gmail.com>
2014-12-08 17:29:54 +0000

    fantasyLeague data updated

--- fantasyLeague.sql
+++ fantasyLeague.sql
@@ -1,13 +1,3 @@
 insert into fantasyLeague (league_id, league_name) values
 ("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main"),
-("l001", "Fantasy League Main");
+("l002", "Spaceballs Only");
