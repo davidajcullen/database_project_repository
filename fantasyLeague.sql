7a1646239f67bff3793267c59d741ca92f5a711f
kvnh <kevinhackett72@gmail.com>
2014-12-08 16:29:18 +0000

    fantasyLeague and fantasyTeamLeague data added to repo

---
+++ fantasyLeague.sql
@@ -0,0 +1,13 @@
+insert into fantasyLeague (league_id, league_name) values
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main"),
+("l001", "Fantasy League Main");
