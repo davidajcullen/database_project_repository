7084c57bed3fc5e53ba3deed68fd3a2d171d859c
kvnh <kevinhackett72@gmail.com>
2014-12-08 16:21:04 +0000

    edited data in player table and added fantasyTeam data file

---
+++ fantasyTeam.sql
@@ -0,0 +1,13 @@
+insert into fantasyTeam (team_id, team_name, user_id) values
+("t001", "Kevin Team 1", "0001"),
+("t002", "Ciaran Team 1", "0002"),
+("t003", "Liam Team 1", "0003"),
+("t004", "Fergus Team 1", "0004"),
+("t005", "Peter Team 1", "0005"),
+("t006", "David Team 1", "0006"),
+("t007", "Kevin Team 2", "0007"),
+("t008", "Ciaran Team 2", "0008"),
+("t009", "Liam Team 2", "0009"),
+("t010", "Fergus Team 2", "0010"),
+("t011", "Peter Team 2", "0011"),
+("t012", "David Team 2", "0012");
