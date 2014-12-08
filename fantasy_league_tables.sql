db256dcad664e91cc82d01699081d897a2a4a041
kvnh <kevinhackett72@gmail.com>
2014-12-08 18:40:44 +0000

    playsIn data added and game_week added as a primary key in squad table - fantasy_league_tables

--- fantasy_league_tables.sql
+++ fantasy_league_tables.sql
@@ -80,8 +80,8 @@ CREATE TABLE squad (
   team_id VARCHAR (15) NOT NULL,
   player_id VARCHAR (15) NOT NULL,
   first_team BOOLEAN NOT NULL DEFAULT 0,
-  game_week VARCHAR (5) NOT NULL, 
-  PRIMARY KEY (team_id),
+  game_week VARCHAR (5) NOT NULL,
+  PRIMARY KEY (team_id, player_id, game_week),
   FOREIGN KEY (team_id)
     REFERENCES fantasyTeam (team_id),
   FOREIGN KEY (player_id)
