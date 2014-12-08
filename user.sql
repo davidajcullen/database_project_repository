d62f41609378b5c8a5427bf58169d4c0d6be968d
kvnh <kevinhackett72@gmail.com>
2014-12-08 16:08:53 +0000

    user data added to repo

---
+++ user.sql
@@ -0,0 +1,13 @@
+insert into user (user_id, first_name, last_name, email) values
+("0001", "Kevin", "Hackett", "khackett03@qub.ac.uk"),
+("0002", "Ciaran", "Gollogly", "cgollogly03@qub.ac.uk"),
+("0003", "Liam", "Connolly", "lconnolly17@qub.ac.uk"),
+("0004", "Fergus", "Taylor", "ftaylor04@qub.ac.uk"),
+("0005", "Peter", "Kilpatrick", "pkilpatrick01@qub.ac.uk"),
+("0006", "David", "Cullen", "dcullen620@qub.ac.uk"),
+("0007", "Kevin", "Hackett", "khackett@gmail.com"),
+("0008", "Ciaran", "Gollogly", "cgollogly@gmail.com"),
+("0009", "Liam", "Connolly", "lconnolly@gmail.com"),
+("0010", "Fergus", "Taylor", "ftaylor@gmail.com"),
+("0011", "Peter", "Kilpatrick", "pkilpatrick@gmail.com"),
+("0012", "David", "Cullen", "dcullen@gmail.com");
