7bd1220eb128e8c34cd85f90f98c0189b3c4642d
kvnh <kevinhackett72@gmail.com>
2014-12-12 21:44:34 +0000

    updated fantasyLeague names and edited overall SQL input file

--- fantasyLeague.sql
+++ fantasyLeague.sql
@@ -1,3 +1,3 @@
 insert into fantasyLeague (league_id, league_name) values
-("l001", "Fantasy League Main"),
-("l002", "Spaceballs Only");
+("l001", "Fantasy League Overall"),
+("l002", "League of Spaceballs");
